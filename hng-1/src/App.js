d5c4578d6dd1222bd82cb8b66fa7059b6dfbebb3
Emmanuel Eze <Emmanuel.Eze@mainone.net>
2022-11-14 14:23:04 +0100

    feat: Building Homepage

---
+++ hng-1/src/App.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='profile'>
+      <img src='NEW PIC.jpg' id="profile__img" className='image'/>
+     <div id='twitter'>ezeemmachukwu</div>
+     <div id='slack'>Emmanuel</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ stage-1-task/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+//import './App.css';
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className='profile_pics'>
+      <img src='NEW PIC.jpg' className='image'/>
+     <div id='slackName'>@Emmanuel</div>
+      </div>
+        
+     
+    </div>
+  );
+}
+
+export default App;
